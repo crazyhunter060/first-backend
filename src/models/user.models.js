90d3c590edcf27096a670f232e763dade6a08816
crazyhunter060 <crazyhunter060@gmail.com>
2026-04-04 16:04:59 +0530

    all setup configured

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -83,8 +83,5 @@ userSchema.methods.generateRefreshToken=function(){
         }
     )
 }
-userSchema.methods.generateRefreshToken=function(){
-    
-}
 
 export const User=mongoose.model("User",userSchema)
